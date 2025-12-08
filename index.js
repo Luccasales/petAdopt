76b5ca0026f8780f0b231116e01b1662ad35f053
Luccasales <lucca.cps@hotmail.com>
2025-12-08 19:13:08 -0300

    HTML da pagina inicial finalizado!

---
+++ index.js
@@ -0,0 +1,35 @@
+// Dados dos pets
+/*
+const nomeDoPet = document.getElementById("nomeDoPet");
+const descPet = document.getElementById("descricao");
+const fotoGrande = document.getElementByID("fotoGrande");
+
+
+const pets = [
+    {
+        nome: "Scooby",
+        descricao: "Scooby é um cão leal e protetor. Ama crianças e é muito obediente. Precisa de espaço para correr e brincar. Ideal para famílias ativas."
+        fotoGrande: "./assets/scoby.png",
+        fotoMini: "./assets/scoby.png"
+    },
+    {
+        nome: "Pantera",
+        descricao: "Pantera é uma gatinha carinhosa e brincalhona. Ela adora carinho e é muito sociável com outros gatos. Procura uma família que lhe dê muito amor e atenção.",
+        fotoGrande: "./assets/pantera.png",
+        fotoMini: "./assets/pantera.png"
+    },
+    {
+        nome: "Thor",
+        descricao: "Thor é um cão cheio de energia e lealdade. Adora brincar e estar perto da família, sempre demonstrando carinho e proteção. Procura um lar onde possa receber amor e ter bastante espaço para gastar sua energia.",
+        fotoGrande: "./assets/thor.png",
+        fotoMini: "./assets/thor.png"
+    },
+    {
+        nome: "Garfield",
+        descricao: "Garfield é um gato independente mas carinhoso. Gosta de explorar e é muito curioso. Ideal para quem quer um companheiro aventureiro.",
+        fotoGrande: "./assets/garfield.png",
+        fotoMini: "./assets/garfield.png"
+    }
+];
+
+*/
