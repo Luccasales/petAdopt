42ff81e2f5caec67707d9fcbefe1945d6b96d34e
Luccasales <lucca.cps@hotmail.com>
2025-12-10 10:42:31 -0300

    Reformulação no HTML e JS finalizado

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+//Vou deixar tudo explicado para nao ter problemas hahaha
 // Dados dos pets
-/*
+//Aqui onde muda o HTML
 const nomeDoPet = document.getElementById("nomeDoPet");
 const descPet = document.getElementById("descricao");
-const fotoGrande = document.getElementByID("fotoGrande");
+const fotoGrande = document.getElementById("fotoGrande");
 
+//miniatura dos pet,para ver onde o usuario clicou,logo adiciono um eventListener
+const scooby = document.getElementById("Scooby");
+const pantera = document.getElementById("Pantera");
+const thor = document.getElementById("Thor");
+const garfield = document.getElementById("Garfield");
 
+//Obejeto com Array para pegar o que vai ser colocado no HTML
 const pets = [
     {
         nome: "Scooby",
-        descricao: "Scooby é um cão leal e protetor. Ama crianças e é muito obediente. Precisa de espaço para correr e brincar. Ideal para famílias ativas."
+        descricao: "Scooby é um cão leal e protetor. Ama crianças e é muito obediente. Precisa de espaço para correr e brincar. Ideal para famílias ativas.",
         fotoGrande: "./assets/scoby.png",
         fotoMini: "./assets/scoby.png"
     },
@@ -32,4 +39,35 @@ const pets = [
     }
 ];
 
-*/
+//Pega o ID é o parametro do eventListener 
+const verificarClique = (id) => {
+    if(id === "Scooby"){
+        mostrarInfo(pets[0]);
+    }
+
+    else if(id === "Pantera"){
+        mostrarInfo(pets[1]);
+    }
+
+    else if(id === "Thor"){
+        mostrarInfo(pets[2]);
+    }
+
+    else if(id === "Garfield"){
+        mostrarInfo(pets[3]);
+    }
+}
+
+//Muda o HTML
+const mostrarInfo = (pet) => {
+    nomeDoPet.textContent = pet.nome;
+    descPet.textContent = pet.descricao;
+    fotoGrande.src = pet.fotoGrande
+}
+
+
+//Adiciona o evento de clique,que chama a função VerificarClique(Com o parametro)
+scooby.addEventListener("click", () => verificarClique("Scooby"));
+pantera.addEventListener("click", () => verificarClique("Pantera"));
+thor.addEventListener("click", () => verificarClique("Thor"));
+garfield.addEventListener("click", () => verificarClique("Garfield"));
