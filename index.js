0805585e8c23bf3be32e362a65f3e045fa447115
Luccasales <lucca.cps@hotmail.com>
2025-12-11 14:54:02 -0300

    Arrumando path das fotos

--- index.js
+++ index.js
@@ -19,26 +19,26 @@ const pets = [
     {
         nome: "Scooby",
         descricao: "Scooby é um cão leal e protetor. Ama crianças e é muito obediente. Precisa de espaço para correr e brincar. Ideal para famílias ativas.",
-        fotoGrande: "./assets/scoby.png",
-        fotoMini: "./assets/scoby.png"
+        fotoGrande: "./Assets/scoby.png",
+        fotoMini: "./Assets/scoby.png"
     },
     {
         nome: "Pantera",
         descricao: "Pantera é uma gatinha carinhosa e brincalhona. Ela adora carinho e é muito sociável com outros gatos. Procura uma família que lhe dê muito amor e atenção.",
-        fotoGrande: "./assets/pantera.png",
-        fotoMini: "./assets/pantera.png"
+        fotoGrande: "./Assets/pantera.png",
+        fotoMini: "./Assets/pantera.png"
     },
     {
         nome: "Thor",
         descricao: "Thor é um cão cheio de energia e lealdade. Adora brincar e estar perto da família, sempre demonstrando carinho e proteção. Procura um lar onde possa receber amor e ter bastante espaço para gastar sua energia.",
-        fotoGrande: "./assets/thor.png",
-        fotoMini: "./assets/thor.png"
+        fotoGrande: "./Assets/thor.png",
+        fotoMini: "./Assets/thor.png"
     },
     {
         nome: "Garfield",
         descricao: "Garfield é um gato independente mas carinhoso. Gosta de explorar e é muito curioso. Ideal para quem quer um companheiro aventureiro.",
-        fotoGrande: "./assets/garfield.png",
-        fotoMini: "./assets/garfield.png"
+        fotoGrande: "./Assets/garfield.png",
+        fotoMini: "./Assets/garfield.png"
     }
 ];
 
